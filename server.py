0a67623e891d25f45a154224f5beecb05126466d
smilliononline <million_18@outlook.com>
2018-06-30 22:17:06 -0500

    update:

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+# import the Connector function
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+# connect and store the connection in "mysql"; note that you pass the database name to the function
+mysql = MySQLConnector(app, 'friendsdb')
+# an example of running a query
+@app.route('/')
+def index():
+    query = "SELECT * FROM friends"
+    friends = mysql.query_db(query)
+    return render_template('index.html', all_friends=friends)
+
+@app.route('/friends', methods=['POST'])
+def create():
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'occupation': request.form['occupation']
+   }
+    mysql.query_db(query, data)
+    return redirect('/')
+
+@app.route('/friends/<friend_id>')
+def show(friend_id):
+    query = "SELECT * FROM friends WHERE id = :specific_id"
+    data = {'specific_id': friend_id}
+    friends = mysql.query_db(query, data)
+    return render_template('index.html', one_friend=friends[0])
+app.run(debug=True)
